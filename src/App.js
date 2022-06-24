1eb916c53a1dd639540a0ae067804ec4b09991e3
Taline <talinedacosta@gmail.com>
2022-06-24 17:57:07 -0300

    Initial Structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
 function App() {
   return (
-    <div className="App">     
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
