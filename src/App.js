e9169f165fccc0c07bdd74398c20f0334fbf0843
Taline <talinedacosta@gmail.com>
2022-06-24 18:57:05 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
