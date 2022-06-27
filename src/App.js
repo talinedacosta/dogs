9cd26afa26426249b1c65ad629ba4565f2a309fa
Taline <talinedacosta@gmail.com>
2022-06-27 13:59:59 -0300

    Login Routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/login/*" element={<Login />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
