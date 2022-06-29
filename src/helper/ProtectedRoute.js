bb70dd32505fcc308b226804f212f13ee9292c78
Taline <talinedacosta@gmail.com>
2022-06-29 16:14:52 -0300

    UserHeader

--- src/helper/ProtectedRoute.js
+++ src/helper/ProtectedRoute.js
@@ -5,8 +5,6 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
   const { login } = React.useContext(UserContext);
 
-  console.log(login);
-
   return login ? children : <Navigate to="/login" />;
 };
 
