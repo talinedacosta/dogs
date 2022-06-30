243c65de052bc1ef90b75dacffb6edf3a761b44d
Taline <talinedacosta@gmail.com>
2022-06-30 19:03:52 -0300

    Feed Modal 1

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../hooks/useFetch';
+import { PHOTO_GET } from '../../api';
+import Error from '../../helper/Error';
+import Loading from '../../helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    const { response } = request(url, options);
+  }, [photo, request]);
+
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
