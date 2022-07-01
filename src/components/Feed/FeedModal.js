94bb8525438800358b07dce82cecd2cf70fcca8b
Taline <talinedacosta@gmail.com>
2022-07-01 14:43:21 -0300

    Feed Modal 2

--- src/components/Feed/FeedModal.js
+++ src/components/Feed/FeedModal.js
@@ -6,7 +6,7 @@ import Error from '../../helper/Error';
 import Loading from '../../helper/Loading';
 import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
@@ -14,8 +14,14 @@ const FeedModal = ({ photo }) => {
     const { response } = request(url, options);
   }, [photo, request]);
 
+  function handleOutsiteClik({ target, currentTarget }) {
+    if (target === currentTarget) {
+      setModalPhoto(null);
+    }
+  }
+
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsiteClik}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
